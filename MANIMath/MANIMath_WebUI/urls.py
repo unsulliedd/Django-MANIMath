353ed8a5c7a62f1d9b21ebc469c011ebfa972967
Berk <berkkarakas98@hotmail.com>
2023-06-11 15:59:28 +0300

    Update home view and URLs for index page

--- MANIMath/MANIMath_WebUI/urls.py
+++ MANIMath/MANIMath_WebUI/urls.py
@@ -1,1 +1,9 @@
 from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Index
+    path('', views.home, name='index'),
+    path('home/', views.home, name='home'),
+    path('index/', views.home, name='home'),
+]
--- MANIMath/MANIMath_WebUI/views.py
+++ MANIMath/MANIMath_WebUI/views.py
@@ -1,1 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from datetime import datetime
+
+def home(request: HttpRequest):
+    return render(request,
+        'MANIMath_WebUI/index.html',
+        {
+            'title': 'Home Page',
+            'year': datetime.now().year,
+        }
+    )
