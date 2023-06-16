4ac0ac661f3b4ac3b9e92df9358fa4eaa6d594fa
Berk <berkkarakas98@hotmail.com>
2023-06-17 02:20:05 +0300

    Add validation for login and register forms

--- MANIMath/MANIMath_Account/forms.py
+++ MANIMath/MANIMath_Account/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth import authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -7,7 +8,41 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'password1', 'password2')
 
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            self.add_error('username', 'This username is already taken.')
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            self.add_error('email', 'This email is already registered.')
+        return email
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get('password1')
+        password2 = cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            self.add_error('password2', 'The passwords do not match.')
+
+
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user is None:
+                try:
+                    User.objects.get(username=username)
+                except User.DoesNotExist:
+                    self.add_error('username', 'Invalid username or user does not exist')
+                else:
+                    self.add_error('password', 'Incorrect password')
