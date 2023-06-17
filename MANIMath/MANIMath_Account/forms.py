61a087c59be3289537c4903292cd8cb8ae84d0fd
Berk <berkkarakas98@hotmail.com>
2023-06-17 15:52:40 +0300

    Updated label suffix for SignUpForm and LoginForm in forms.py

--- MANIMath/MANIMath_Account/forms.py
+++ MANIMath/MANIMath_Account/forms.py
@@ -4,9 +4,18 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
-    class Meta:
+    class Meta(UserCreationForm.Meta):
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        labels = {
+            'username': 'Username',
+            'email': 'Email',
+            'password1': 'Password',
+            'password2': 'RePassword',
+        }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ""
 
     def clean_username(self):
         username = self.cleaned_data.get('username')
@@ -29,8 +38,12 @@ class SignUpForm(UserCreationForm):
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(label='Username')
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        self.label_suffix = ''
 
     def clean(self):
         cleaned_data = super().clean()
