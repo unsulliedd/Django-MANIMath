455b90e3f38c7035bcf53f4b1dd7a2afc4a4c9fe
Berk <berkkarakas98@hotmail.com>
2023-06-18 13:16:25 +0300

    Add topic_list view function and updated URL patterns for topic listing

--- MANIMath/MANIMath_WebUI/urls.py
+++ MANIMath/MANIMath_WebUI/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('', views.home, name='index'),
     path('home', views.home, name='home'),
     path('index/', views.home, name='home'),
+
+    # Topic List
+    path('topics', views.topic_list, name='topic_list'),
+    path('topics/<str:category>', views.topic_list, name='topic_list_category'),
 ]
--- MANIMath/MANIMath_WebUI/views.py
+++ MANIMath/MANIMath_WebUI/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 from datetime import datetime
+from .models import *
 
 def home(request: HttpRequest):
     return render(request,
@@ -8,5 +9,31 @@ def home(request: HttpRequest):
         {
             'title': 'Home Page',
             'year': datetime.now().year,
+        }
+    )
+
+def topic_list(request):
+    topics = Topic.objects.all()
+    categories = Category.objects.all()
+
+    query = request.GET.get('q', '')
+    category = request.GET.get('category', '')
+
+    if query:
+        topics = topics.filter(name__icontains=query)
+        categories = categories.filter(name__icontains=query)
+
+    if category:
+        topics = topics.filter(category__name=category)  
+
+    return render(
+        request, 
+        'MANIMath_WebUI/topics.html',
+        {
+            'title': 'Topics',
+            'year': datetime.now().year,
+            'topics': topics,
+            'categories': categories,
+            'query': query
         }
     )
