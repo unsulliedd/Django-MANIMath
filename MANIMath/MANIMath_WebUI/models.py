9c90e94f1fc7c59da02e1db272f22a3f480c90b2
Berk <berkkarakas98@hotmail.com>
2023-06-18 13:00:46 +0300

    Add Category model verbose_name

--- MANIMath/MANIMath_WebUI/models.py
+++ MANIMath/MANIMath_WebUI/models.py
@@ -4,6 +4,10 @@ class Category(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return self.name
 
