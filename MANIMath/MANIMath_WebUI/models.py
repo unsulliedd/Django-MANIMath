eb2e46d04d2956dc681980b01aa7436da7844962
Berk <berkkarakas98@hotmail.com>
2023-06-18 12:56:11 +0300

    Add Topic and Category model

--- MANIMath/MANIMath_WebUI/models.py
+++ MANIMath/MANIMath_WebUI/models.py
@@ -1,1 +1,24 @@
 from django.db import models
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Topic(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True, default=None)
+    defination_url = models.URLField(blank=True, null=True)
+    image_url = models.URLField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'Topic'
+        verbose_name_plural = 'Topics'
+
+    def __str__(self):
+        return self.name
