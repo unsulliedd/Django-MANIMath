2068bca6212eddd47e5b804a8c640cbbd9361e12
Berk <berkkarakas98@hotmail.com>
2023-06-25 18:16:01 +0300

    Added RootFindingModel

--- MANIMath/MANIMath_Data/models.py
+++ MANIMath/MANIMath_Data/models.py
@@ -24,3 +24,25 @@ class FunctionModel(models.Model):
 
     def __str__(self):
         return f"{self.id} - {self.user.username.capitalize()} - {self.topic.name.capitalize()}"
+
+class RootFindingModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    custom_animation_name = models.CharField(max_length=256, blank=True, null=True)
+    equation1  = models.CharField(max_length=100, blank=True, null=True)
+    equation2  = models.CharField(max_length=100, blank=True, null=True)
+    create_date = models.DateTimeField(default=timezone.now, editable=False)
+
+    def save(self, *args, **kwargs):
+        if not self.custom_animation_name:
+            self.custom_animation_name = f"{self.topic.name.capitalize()}-{self.id}"
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Search Algorithm Model'
+        verbose_name_plural = 'Search Algorithm Model'
+
+    def __str__(self):
+        return f"{self.id} - {self.user.username.capitalize()} - {self.topic.name.capitalize()}"
