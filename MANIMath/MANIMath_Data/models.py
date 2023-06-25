5dd7722959c95c4989af886d555709fc0c7e5289
Berk <berkkarakas98@hotmail.com>
2023-06-25 18:14:39 +0300

    Added FunctionModel

--- MANIMath/MANIMath_Data/models.py
+++ MANIMath/MANIMath_Data/models.py
@@ -1,2 +1,26 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from MANIMath_WebUI.models import *
 
+class FunctionModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    custom_animation_name = models.CharField(max_length=256, blank=True, null=True)
+    equation1  = models.CharField(max_length=100, blank=True, null=True)
+    equation2  = models.CharField(max_length=100, blank=True, null=True)
+    create_date = models.DateTimeField(default=timezone.now, editable=False)
+
+    def save(self, *args, **kwargs):
+        if not self.custom_animation_name:
+            self.custom_animation_name = f"{self.topic.name.capitalize()}-{self.id}"
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Function Model'
+        verbose_name_plural = 'Function Model'
+
+    def __str__(self):
+        return f"{self.id} - {self.user.username.capitalize()} - {self.topic.name.capitalize()}"
