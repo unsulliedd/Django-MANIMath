1ce22ac06e6eb250840c7dc753b138ceaf097dd4
Berk <berkkarakas98@hotmail.com>
2023-06-25 18:35:10 +0300

    Registered MANIMath_Data models to admin site

--- MANIMath/MANIMath_Data/admin.py
+++ MANIMath/MANIMath_Data/admin.py
@@ -1,1 +1,35 @@
 from django.contrib import admin
+from .models import *
+
+class FunctionModelAdmin(admin.ModelAdmin):
+    readonly_fields = ('formatted_create_date',)
+    def formatted_create_date(self, obj):
+        return obj.create_date.strftime('%Y-%m-%d %H:%M:%S')
+
+    formatted_create_date.short_description = 'Create Date'
+
+class RootFindingModelAdmin(admin.ModelAdmin):
+    readonly_fields = ('formatted_create_date',)
+    def formatted_create_date(self, obj):
+        return obj.create_date.strftime('%Y-%m-%d %H:%M:%S')
+
+    formatted_create_date.short_description = 'Create Date'
+
+class SortModelAdmin(admin.ModelAdmin):
+    readonly_fields = ('formatted_create_date',)
+    def formatted_create_date(self, obj):
+        return obj.create_date.strftime('%Y-%m-%d %H:%M:%S')
+
+    formatted_create_date.short_description = 'Create Date'
+
+class SearchModelAdmin(admin.ModelAdmin):
+    readonly_fields = ('formatted_create_date',)
+    def formatted_create_date(self, obj):
+        return obj.create_date.strftime('%Y-%m-%d %H:%M:%S')
+
+    formatted_create_date.short_description = 'Create Date'
+
+admin.site.register(FunctionModel,FunctionModelAdmin)
+admin.site.register(RootFindingModel,RootFindingModelAdmin)
+admin.site.register(SearchModel,SearchModelAdmin)
+admin.site.register(SortModel,SortModelAdmin)
