3d006ee3b76da0b9fd31c552b600c05b0782c334
Berk <berkkarakas98@hotmail.com>
2023-06-29 17:50:10 +0300

    Update main URLs to include MANIMath_Api URLs

--- MANIMath/MANIMath/urls.py
+++ MANIMath/MANIMath/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin', admin.site.urls),
-    path('', include("MANIMath_Account.urls")),
+    path('', include('MANIMath_Account.urls')),
     path('', include('MANIMath_WebUI.urls')),
+    path('', include('MANIMath_Api.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
