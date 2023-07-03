46f3a9c6b0c9364f312398a72660e7f45a39526a
Berk <berkkarakas98@hotmail.com>
2023-07-03 20:43:12 +0300

    Added token creation for new users

--- MANIMath/MANIMath_Account/signals.py
+++ MANIMath/MANIMath_Account/signals.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from .models import Profile 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -12,3 +13,8 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
